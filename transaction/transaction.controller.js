e3b3429f144e2da6e59381d7bbf751f94b1378bd
vladoskin1998 <vladosik4891@gmail.com>
2022-08-24 21:21:48 +0300

    add route amd controller

--- transaction/transaction.controller.js
+++ transaction/transaction.controller.js
@@ -1,1 +1,25 @@
 // const { checkSchema, validationResult } = require('express-validator');
+class TransactionController{
+
+    async getBalance(req,res){
+
+    }
+
+    async getHistory(req,res){
+
+    }
+
+
+    async changeBalance(req,res){
+
+    }
+
+
+    async remittance(req,res){
+
+    }
+
+
+}
+
+export default new TransactionController()
--- transaction/transaction.router.js
+++ transaction/transaction.router.js
@@ -1,5 +1,14 @@
 import { Router } from 'express';
+import TransactionController from './transaction.controller.js';
 
 const TransactionRouter = new Router();
+
+TransactionRouter.get('/get-balance', TransactionController.getBalance)
+
+TransactionRouter.get('/get-history', TransactionController.getHistory)
+
+TransactionRouter.post('/change-balance', TransactionController.changeBalance)
+
+TransactionRouter.post('/remittance', TransactionController.remittance)
 
 export default TransactionRouter
