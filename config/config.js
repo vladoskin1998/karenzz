41c96a85c324a5b77e197db147f10cba49ea368a
vladoskin1998 <vladosik4891@gmail.com>
2022-08-25 20:26:09 +0300

    swagger method

---
+++ config/config.js
@@ -0,0 +1,3 @@
+export const MESSEGE_OK="OK"
+export const NEW_USER="NEW USER"
+export const BALANCE_NEGATIVE="Balance is negative"
--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use('/api', router);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const server = async () => {
-  const db = await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@transaction.osqhvw2.mongodb.net/?retryWrites=true&w=majority`,
-    { useUnifiedTopology: true, useNewUrlParser: true });
+ // const db = await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@transaction.osqhvw2.mongodb.net/?retryWrites=true&w=majority`,
+    //{ useUnifiedTopology: true, useNewUrlParser: true });
   try {
     app.listen(process.env.PORT, async () => { console.log(`Server listening on port ${process.env.PORT}!`); });
   } catch (error) {
